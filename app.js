b037f1720a389e2f58c0798ceeacae5fdc540ee7
sudheerrkv <sudheerrgukt37@gmail.com>
2022-07-03 09:29:56 +0530

    sudheer

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ res.send("<h2>Welcome to Node JS express app</h2>"+appEnv.url+appEnv.port+port+p
 console.log(__dirname+"/images/mithunlogo.jpg");
 */
 app.get('/mithuntechnologies', function(request, response) {
-    //response.send("<h2><center>Welcome to Node JS app</h2>");
+    //response.send("<h2><center>Welcome to Node JS app sudheer</h2>");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
     response.write("<h2><center>Welcome to  Mithun Technologies. Please Contact +91-9980923226 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
